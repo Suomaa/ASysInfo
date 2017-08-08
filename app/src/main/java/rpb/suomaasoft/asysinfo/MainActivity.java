da5d61be0f9076f6102d6ef79bb668fa6f8776bb
Suomaa <a.antomonov@mail.ru>
2017-08-08 12:16:08 +0300

    Add ASysInfo pre-version

---
+++ app/src/main/java/rpb/suomaasoft/asysinfo/MainActivity.java
@@ -0,0 +1,43 @@
+package rpb.suomaasoft.asysinfo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.getSystemInfo);
+        final TextView textView = (TextView)findViewById(R.id.info_display);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(
+                        "SERIAL: " + Build.SERIAL + "\n" +
+                                "MODEL: " + Build.MODEL + "\n" +
+                                "ID: " + Build.ID + "\n" +
+                                "Manufacture: " + Build.MANUFACTURER + "\n" +
+                                "Brand: " + Build.BRAND + "\n" +
+                                "Type: " + Build.TYPE + "\n" +
+                                "User: " + Build.USER + "\n" +
+                                "BASE: " + Build.VERSION_CODES.BASE + "\n" +
+                                "INCREMENTAL: " + Build.VERSION.INCREMENTAL + "\n" +
+                                "SDK:  " + Build.VERSION.SDK + "\n" +
+                                "BOARD: " + Build.BOARD + "\n" +
+                                "BRAND: " + Build.BRAND + "\n" +
+                                "HOST: " + Build.HOST + "\n" +
+                                "FINGERPRINT: "+Build.FINGERPRINT + "\n" +
+                                "Version Code: " + Build.VERSION.RELEASE
+                );
+            }
+        });
+    }
+}
